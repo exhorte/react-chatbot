9902a7de1cbf56daed05d10c8c415c2b1a992e86
exhorte <exhortemboumbaba@gmail.com>
2026-04-02 19:04:17 +0100

    Update Sidebar, Messages, Chat components and UI styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Assistant as AssistantClass } from "./assistants/anthropicai";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Loader } from "./components/Loader/Loader";
-import { Chat } from "./components/Chat/Chat";
+import { Messages } from "./components/Messages/Messages";
 import { Controls } from "./components/Controls/Controls";
 import { Assistant } from "./components/Assistant/Assistant";
 import { Theme } from "./components/Theme/Theme";
@@ -73,17 +74,24 @@ function App() {
         <img className={styles.Logo} src="/chat-bot.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer}>
-        <Chat messages={messages} />
-      </div>
-      <Controls
-        isDisabled={isLoading || isStreaming}
-        onSend={handleContentSend}
-      />
-      <div className={styles.Configuration}>
-        <Assistant onAssistantChange={handleAssistantChange} />
-        <Theme />
-      </div>
+
+     <div className={styles.Content}>
+        <Sidebar />
+
+        <main className={styles.Main}>
+          <div className={styles.ChatContainer}>
+            <Messages messages={messages} />
+          </div>
+          <Controls
+            isDisabled={isLoading || isStreaming}
+            onSend={handleContentSend}
+          />
+          <div className={styles.Configuration}>
+            <Assistant onAssistantChange={handleAssistantChange} />
+            <Theme />
+          </div>
+        </main>
+     </div>
     </div>
   );
 }
